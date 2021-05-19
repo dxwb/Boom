97179ab4b1c44f48c07ed0e5717156c0e7e8e355
dxwb <dx-wb@qq.com>
2021-05-20 07:12:24 +0800

    修改头像圆角

--- script/Avatar.js
+++ script/Avatar.js
@@ -3,13 +3,11 @@
     create(x = 0, y = 0) {
       return Bodies.rectangle(x, y, 40, 40, {
         chamfer: {
-          radius: 3
+          radius: 5
         },
         render: {
           sprite: {
-            texture: '../images/avatar.jpg',
-            xScale: .5,
-            yScale: .5
+            texture: '../images/avatar.jpg'
           }
         }
       })
